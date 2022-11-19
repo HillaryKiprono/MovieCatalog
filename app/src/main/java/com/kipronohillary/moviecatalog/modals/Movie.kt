b194b11e7a37cd2ce6115683a999d138bde9b7a9
Hillary Kiprono <kipronohillary240@gmail.com>
2022-11-19 10:52:32 +0300

    movie class

---
+++ app/src/main/java/com/kipronohillary/moviecatalog/modals/Movie.kt
@@ -0,0 +1,23 @@
+package com.kipronohillary.moviecatalog.modals
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Movie(
+    @SerializedName("id")
+    val id : String ?,
+
+    @SerializedName("title")
+    val title : String?,
+
+    @SerializedName("poster_path")
+    val poster : String?,
+
+    @SerializedName("release_date")
+    val release : String?
+
+) : Parcelable {
+    constructor() : this("", "", "", "")
+}
